38e3d224cdefa823a8ee3d2c08bf52912d8bbae6
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-03-28 00:20:20 +0600

    adding file

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Practic_try_catch
+{
+    class Program
+    {
+        const int five = 5;
+        const int ten = 10;
+        static int amount = 0;
+        static void Main(string[] args)
+        {
+            int[] array = new int[five];
+            bool isParse;
+
+            WriteLine("Здраствуйте, введите 5 цифр: \n");
+            for (int i = 0; i < five; i++)
+            {
+                isParse = int.TryParse(ReadLine(), out array[i]);
+            }
+
+            CheckProblem(array);
+
+            ReadKey();
+        }
+
+        private static void CheckProblem(int[] array)
+        {
+            try
+            {
+                for (int i = 0; i < ten; i++)
+                {
+                    amount = amount + array[i];
+                }
+            }
+            catch (IndexOutOfRangeException indexOutOfRangeException)
+            {
+                WriteLine(indexOutOfRangeException);
+            }
+            catch (ArithmeticException arithmeticException)
+            {
+                WriteLine(arithmeticException);
+            }
+            finally
+            {
+                WriteLine("\n Завершение обработки массива");
+            }
+        }
+    }
+}
